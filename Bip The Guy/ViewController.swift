0b84437d1a386f96d91283937057b4eee6b9525d
Emma Loughlin <loughlem@bc.edu>
2018-09-18 02:37:42 -0400

    Initial Commit

---
+++ Bip The Guy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bip The Guy
+//
+//  Created by Emma Loughlin on 9/18/18.
+//  Copyright © 2018 Emma Loughlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
